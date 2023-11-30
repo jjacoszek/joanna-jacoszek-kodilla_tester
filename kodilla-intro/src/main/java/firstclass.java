96d818b6dbc17ad85b75107efbd6d5267d53280d
Joanna Jacoszek <joannazjacoszek@gmail.com>
2023-11-30 20:22:52 +0100

    first class

---
+++ kodilla-intro/src/main/java/firstclass.java
@@ -0,0 +1,13 @@
+public class firstclass {
+    public static void main(String[] args) {
+        String greeting = "Hello World!";
+        String name = "Joanna";
+        int age = 34;
+        char firstletter = 'J';
+
+        System.out.println(greeting);
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(firstletter);
+    }
+}
