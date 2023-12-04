a1569e4d52c1175396bab666635fc4fd829baf72
Joanna Jacoszek <joannazjacoszek@gmail.com>
2023-12-04 15:33:24 +0100

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+    String season = "Winter";
+    int dayOfChristmasEve = 24;
+    boolean isItSnow = true;
+    char firstNameOfChristmas = 'C';
+    double heightOfChristmasTree = 2.46;
+    String wishes = "Merry Christmas";
+
+        System.out.println(season);
+        System.out.println(dayOfChristmasEve);
+        System.out.println(isItSnow);
+        System.out.println(firstNameOfChristmas);
+        System.out.println(heightOfChristmasTree);
+        System.out.println(wishes);
+}
+}
